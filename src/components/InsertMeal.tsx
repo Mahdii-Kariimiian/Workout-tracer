1dc973b4dc14a7a1cdb8c98214e114bb3e0a5340
Mahdi Karimian <mahdii.kariimiian@gmail.com>
2024-10-14 19:09:31 +0200

    add Types

--- src/components/InsertMeal.tsx
+++ src/components/InsertMeal.tsx
@@ -1,6 +1,20 @@
 import { useContext, useState } from "react";
 import { Context } from "../App";
 
+type InsertMealTypes = {
+    setIsModal: React.Dispatch<React.SetStateAction<boolean>>;
+    name: string;
+    setName: React.Dispatch<React.SetStateAction<number | null>>;
+    protein: number;
+    setProtein: React.Dispatch<React.SetStateAction<number | null>>;
+    fat: number;
+    setFat: React.Dispatch<React.SetStateAction<number | null>>;
+    sum: number;
+    setSum: React.Dispatch<React.SetStateAction<number | null>>;
+    carbs: number;
+    setCarbs: React.Dispatch<React.SetStateAction<number | null>>;
+};
+
 const InsertMeal = () => {
     const {
         setIsModal,
@@ -14,7 +28,7 @@ const InsertMeal = () => {
         setSum,
         carbs,
         setCarbs,
-    } = useContext(Context);
+    } = useContext<InsertMealTypes>(Context);
 
     return (
         <div className="p-10 bg-blue-500">
